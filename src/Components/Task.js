168e290c3f456ace5824406d8e2eaad87fb6c8d0
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2023-01-02 22:07:18 +0100

    Fix update feature

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,58 +1,81 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleCheck , faPen } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch } from 'react-redux';
-import { finishToDo } from '../JS/actions/toDoActions';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck, faPen } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { finishToDo, updateToDo } from "../JS/actions/toDoActions";
 
 const Task = ({ task, index, setUpdateData }) => {
   const dispatch = useDispatch();
-
+  const [editedText, setEditedText] = useState(task.description);
+  const [edit, setEdit] = useState(false);
   const markDone = () => {
     dispatch(finishToDo(task.id));
-  }
+  };
+  const handleEdit = () => {
+    dispatch(updateToDo(task.id, editedText));
+    setEdit(false);
+  };
+  const cancelUpdate = () => {
+    setEdit(false);
+    setEditedText(task.description);
+  };
 
-  
   return (
     <>
-        
-        
-            
-              
-              <React.Fragment key={task.id}>
-                
-                  <div className="col taskBg">
-                    <div className={ task.isDone ? 'done' : '' }>
-                      <span className="taskNumber">{index + 1}</span>
-                      <span className="taskText">{task.description}</span>
-                    </div>
-                    <div className="iconsWrap">
-                      <span title="Completed / Not Completed"
-                        onClick={ (e) => markDone(task.id) }
-                      >
-                        <FontAwesomeIcon icon={faCircleCheck} />
-                      </span>
-                    {/*  if task isDone then don't show edit icon */}
-                      {task.isDone ? null : (
-                        <span title="Edit"
-                          onClick={ () => setUpdateData({ 
-                            id: task.id, 
-                            description: task.description, 
-                            isDone: task.isDone ? true : false
-                          }) }
-                        >
-                          <FontAwesomeIcon icon={faPen} />
-                        </span>
-                      )}
-
-                    </div>
+      <React.Fragment key={task.id}>
+        <div className="col taskBg">
+          <div className={task.isDone ? "done" : ""}>
+            <span className="taskNumber">{index + 1}</span>
+            {edit ? (
+              <>
+                <div className="row">
+                  <div className="col">
+                    <input
+                      type="text"
+                      value={editedText}
+                      onChange={(e) => setEditedText(e.target.value)}
+                      className="form-control form-control-lg"
+                    />
                   </div>
-              </React.Fragment>
-              
-              
-            
-          
-    </>
-  )
-}
 
-export default Task
+                  <div className="col-auto">
+                    <button
+                      onClick={handleEdit}
+                      className="btn btn-lg btn-success mr-20"
+                    >
+                      Update
+                    </button>
+                    <button
+                      onClick={cancelUpdate}
+                      className="btn btn-lg btn-warning"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+                <br />
+              </>
+            ) : (
+              <span className="taskText">{task.description}</span>
+            )}
+          </div>
+          <div className="iconsWrap">
+            <span
+              title="Completed / Not Completed"
+              onClick={(e) => markDone(task.id)}
+            >
+              <FontAwesomeIcon icon={faCircleCheck} />
+            </span>
+            {/*  if task isDone then don't show edit icon */}
+
+            <span title="Edit" onClick={() => setEdit(true)}>
+              <FontAwesomeIcon icon={faPen} />
+            </span>
+          </div>
+        </div>
+      </React.Fragment>
+    </>
+  );
+};
+
+export default Task;
--- src/JS/actions/toDoActions.js
+++ src/JS/actions/toDoActions.js
@@ -1,22 +1,26 @@
-import { ADD_TODO, FINISH_TODO, UPDATE_TODO } from "../actionTypes/toDoActionTypes"
+import {
+  ADD_TODO,
+  FINISH_TODO,
+  UPDATE_TODO,
+} from "../actionTypes/toDoActionTypes";
 
 export const addToDo = (newTask) => {
-    return {
-        type: ADD_TODO,
-        payload : newTask
-    }
-}
+  return {
+    type: ADD_TODO,
+    payload: newTask,
+  };
+};
 
 export const finishToDo = (id) => {
-    return {
-        type: FINISH_TODO,
-        payload : id
-    }
-}
+  return {
+    type: FINISH_TODO,
+    payload: id,
+  };
+};
 
-export const updateToDo = (id , description ) => {
-    return {
-        type: UPDATE_TODO,
-        payload : id , description
-    }
-}
+export const updateToDo = (id, updatedDescription) => {
+  return {
+    type: UPDATE_TODO,
+    payload: { id, updatedDescription },
+  };
+};
